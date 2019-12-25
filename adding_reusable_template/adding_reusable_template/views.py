d0359479bc1993bc6b7fae254c208913e2f5a314
NiteshTyagi <nitesh.tyagi.cs.2015@miet.ac.in>
2019-12-25 17:01:32 +0530

    [ADD] adding_reusable_template

---
+++ adding_reusable_template/adding_reusable_template/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import HttpResponse,render
+
+
+def index_bootstrap(request):
+    return render(request,'index.html')
+
+def basic_template(request):
+    return render(request,'base.html')
